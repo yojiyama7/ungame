37f37bcf7338e1946ed3ec5a45e4f8b9f58bd8ab
yayo256 <>
2018-09-10 07:30:15 +0900

    questions print

--- ungame.py
+++ ungame.py
@@ -1,1 +1,23 @@
-print("Hello Ungame!")
+import random
+
+class Question:
+	def __init__(self, id_num, text):
+		self.id_num = id_num
+		self.text = text
+
+if __name__ == "__main__":
+
+	with open("questions.txt", "r", encoding="utf-8") as f:
+		lines = f.read().split("\n")
+
+	while "" in lines:
+		lines.remove("")
+	
+	questions = []
+	for line in lines:
+		id_num, text = line.split("|")
+		questions.append(Question(int(id_num), text))
+
+	random.shuffle(questions)
+	for question in questions:
+		print(question.text)
