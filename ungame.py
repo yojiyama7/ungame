ab8471b6b3c59669950341975a54c2ad9251f078
yayo256 <>
2018-09-12 18:03:56 +0900

    all refact

--- ungame.py
+++ ungame.py
@@ -2,8 +2,7 @@ import random
 import discord
 from ungame_classes import *
 
-END_MESSAGE = "This is the end of the game.\nThank you for playing!"
-phrase = {"join":"join", "next":"next"}
+games = UngameList()
 
 client = discord.Client()
 
@@ -14,46 +13,11 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-
-
 @client.event
 async def on_message(message):
-
-    if message.content.startswith("/"):
-        cmd = message[1:].split(" ")
-
-        if cmd[0] == "ungame":
-
-            if cmd[1] == "phrase":
-                if cmd[2] == "join":
-                    join_phrase = cmd[3]
-                elif cmd[2] == "next":
-                    next_phrase = cmd[3]
-
-            elif cmd[1] == "open": # ここからgame開始
-
-
-            elif cmd[1] == "start":
-                deck = Deck(take_questions())
-
-                while True:
-                    if deck.questions == []:
-                        await client.send_message(message.channel, END_MESSAGE)
-                        break
-
-                    elif message.content == start_message:
-                        await client.send_message(message.channel, deck.draw())
-
-            elif cmd[1] == "end":
-
-                await client.send_message(message.channel, END_MESSAGE)
-
-
-
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await client.send_message(message.channel, m)
+    if message.author != client.user:    
+        return_message = games.command(message)
+        if return_message:
+            await client.send_message(message.channel, return_message)
 
 client.run(take_token())
--- ungame_classes.py
+++ ungame_classes.py
@@ -19,32 +19,98 @@ def take_token():
 def take_questions():
 	return _questions
 
-class Deck:
-	def __init__(self, questions):
-		self.questions = random.shuffle(questions)
+class Phrase:
+	def __init__(self, key, text):
+		self.key = key
+		self.text = text
+	
+	def set_key(self, key):
+		self.key = key
+	def set_phrase(self, phrase):
+		self.text = text
 
-	def draw(self):
-		return self.questions.pop()
-	def is_empty(self):
-		return self.questions == []
+class PhraseList:
+	def __init__(self, phrases):
+		self.phrases = [Phrase(*phrase) for phrase in phrases]
+	
+	def get_by_key(self, key):
+		return list(filter(lambda x: x.key==key, self.phrases))[0]
+	def get_by_text(self, text):
+		return list(filter(lambda x: x.text==text, self.phrases))[0]
+	def get_keys(self):
+		return [phrase.key for phrase in self.phrases]
+	def get_texts(self):
+		return [phrase.text for phrase in self.phrases]
 
 class Ungame:
 	def __init__(self):
-		self.deck = Deck()
-	
-	def start(self, players):
-		self.players = players
-		self.player_num = 0
-		self.next()
+		self.phrase = PhraseList([("join", "join"), ("next", "next")])
+		self.questions = take_questions()
+		random.shuffle(self.questions)
+		self.state = False
 
-	def next(self):
-		if self.deck.is_empty():
-			return False
-		else:
-			question = self.deck.draw()
-			player = self.players[self.player_num]
-			self.player_num += 1
-			return "to " + player.name + "\n" + question.text
+	def open(self):
+		self.state = "open"
+		self.users = []
+		return "募集を開始しました。"
+	def join(self, user):
+		self.users.append(user)
+		return f"{user.name}さんが参加しました。"
+	def question(self):
+		question = self.questions.pop()
+		return f"{question.text}\n{self.users[self.user_num].name}さん答えてください。"
+	def start(self):
+		if self.state != "playing":
+			if len(self.users) != 0:
+				self.user_num = 0
+				self.state = "playing"
+				return "ゲームを開始します。\n" + self.question()
+			else:
+				return "参加者がいません。"
+		return "すでにゲームが開始されています。"
+	def next(self, user):
+		if self.state == "playing":
+			if self.users[self.user_num] == user:
+				self.user_num = (self.user_num+1) % len(self.users)
+				return "--------\n" + self.question()
+	def end(self):
+		self.state = False
+		return "ゲームを終了しました。"
+	def command(self, message):
+		command = message.content
+		if "/" == command[0]:
+			cmd = command[1:].split(" ")
+			if "ungame" == cmd[0]:
+				if "phrase" == cmd[1]:
+					return self.set_phrase(cmd[2], cmd[3])
+				elif "open" == cmd[1]:
+					return self.open()
+				elif "start" == cmd[1]:
+					return self.start()
+				elif "stop" == cmd[1]:
+					return self.end()
+		elif command in self.phrase.get_texts():
+			phrase_key = self.phrase.get_by_text(command).key
+			if "join" == phrase_key:
+				return self.join(message.author)
+			elif "next" == phrase_key:
+				return self.next(message.author)
+
+class UngameList:
+	def __init__(self, ungames=dict()):
+		self.ungames = ungames
+	def __getitem__(self, key):
+		return self.ungames[key]
+
+	def add_ungame(self, key):
+		self.ungames[key] = Ungame()
+	def pop_ungame(self, key):
+		self.ungames.pop(key)
+	def command(self, message):
+		channel = message.channel
+		if channel not in self.ungames:
+			self.add_ungame(channel)
+		return self[channel].command(message)
 
 if __name__ == "__main__":
 
